6465e420f4000bb2567ebbdbeaf6978c4338d423
HyunSeoChoi <arenaofjagal@naver.com>
2019-07-23 23:28:28 +0900

    백준 14492번

---
+++ BOJ/14492.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+bool a[301][301],b[301][301];
+
+int main()
+{
+    int n;
+    int cnt = 0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            int k = 0;
+            for(;k<n;k++)
+            {
+                if(a[i][k]&&b[k][j])
+                {
+                    break;
+                }
+            }
+            if(k < n)
+            {
+                cnt ++;
+            }
+        }
+    }
+    printf("%d",cnt);
+}
