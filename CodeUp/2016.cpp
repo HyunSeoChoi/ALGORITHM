0f72e0e7bda0b2894360b131ba2b23a60dd6e740
HyunSeoChoi <arenaofjagal@naver.com>
2019-07-27 22:14:26 +0900

    코드업 2016번

---
+++ CodeUp/2016.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int arr[1000000];
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    sort(arr,arr+n);
+    printf("%d %d",arr[0],arr[n-1]);
+}
