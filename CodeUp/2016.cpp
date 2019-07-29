ed4caa064b3c47f797887e9fb3058ac847691da7
HyunSeoChoi <arenaofjagal@naver.com>
2019-07-29 22:39:26 +0900

    백준 7567

--- CodeUp/2016.cpp
+++ CodeUp/2016.cpp
@@ -1,18 +1,30 @@
 #include <cstdio>
+#include <cmath>
 #include <algorithm>
+#include <cmath>
+#include <limits>
+#include <cstring>
 
 using namespace std;
 
-int arr[1000000];
+char str[1000];
 
 int main()
 {
     int n;
-    scanf("%d",&n);
-    for(int i=0;i<n;i++)
+    scanf("%d", &n);
+
+    scanf("%s", str);
+
+
+    for(int i=0;i<strlen(str);i++)
     {
-        scanf("%d",&arr[i]);
+        if(i%3 == n%3 && i!=0)
+        {
+            printf(",");
+        }
+
+        printf("%c", str[i]);
     }
-    sort(arr,arr+n);
-    printf("%d %d",arr[0],arr[n-1]);
 }
+
