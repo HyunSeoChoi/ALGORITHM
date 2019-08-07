8b5d707137521e5165118149ad57ece98f888ebc
HyunSeoChoi <arenaofjagal@naver.com>
2019-08-08 02:02:23 +0900

    백준 2750번

---
+++ BOJ/2750.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+}
