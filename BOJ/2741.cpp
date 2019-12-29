8087f52ca73a2cdb2fd5741598b8e5fb8c00a87c
HyunSeoChoi <arenaofjagal@gmail.com>
2019-12-29 15:56:32 +0900

    백준 2741번

---
+++ BOJ/2741.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    scanf("%d", &x);
+
+    for(int i=1;i<=x;i++)
+    {
+        printf("%d\n", i);
+    }
+}
