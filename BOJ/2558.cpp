f033d858741c3ed8f7fa0836c83a4aa9afb1bddb
HyunSeoChoi <arenaofjagal@gmail.com>
2019-12-30 22:54:30 +0900

    백준 2558번

---
+++ BOJ/2558.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <queue>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    scanf("%d %d", &a, &b);
+
+    printf("%d", a+b);
+}
