a2badcb473c0f33f4a6803060b70ba873bf4e9f6
HyunSeoChoi <arenaofjagal@gmail.com>
2020-01-01 01:58:59 +0900

    백준 2743번

---
+++ BOJ/2743.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <cstring>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    char str[101];
+    scanf("%s", str);
+    printf("%d", strlen(str));
+}
